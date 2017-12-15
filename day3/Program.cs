0bb377506b231bb9a13d21d19d50498e6aef45bf
Martin <mknack@gmx.net>
2017-12-15 10:40:15 +0100

    aargh

--- day3/Program.cs
+++ day3/Program.cs
@@ -7,6 +7,14 @@ using System.Threading.Tasks;
 namespace day3 {
     class Program {
         static void Main(string[] args) {
+            int input = 265149;
+            int[,] dings = new int[515, 515];
+            dings[207, 207] = 1;
+            dings[207, 208] = 2;
+            dings[206, 208] = 3;
+            dings[206, 207] = 4;
+
+            Console.ReadKey();
         }
     }
 }
